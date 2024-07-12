e47f0ae1c0a048e86c0e3101a40fa251c55eabf8
sirkaue <kauekdslopes@gmail.com>
2024-07-12 14:27:16 -0300

    refatorando controller

--- src/main/java/com/sirkaue/forumHub/forum_hub/controller/TopicoController.java
+++ src/main/java/com/sirkaue/forumHub/forum_hub/controller/TopicoController.java
@@ -1,17 +1,15 @@
 package com.sirkaue.forumHub.forum_hub.controller;
 
-import com.sirkaue.forumHub.forum_hub.domain.topico.Topico;
-import com.sirkaue.forumHub.forum_hub.domain.topico.TopicoRepository;
 import com.sirkaue.forumHub.forum_hub.domain.topico.dto.DadosCadastroTopico;
 import com.sirkaue.forumHub.forum_hub.domain.topico.dto.DadosDetalhamentoTopico;
 import com.sirkaue.forumHub.forum_hub.domain.topico.dto.DadosListagemTopico;
+import com.sirkaue.forumHub.forum_hub.service.TopicoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -22,28 +20,24 @@ import java.net.URI;
 public class TopicoController {
 
     @Autowired
-    private TopicoRepository repository;
+    private TopicoService service;
 
     @PostMapping
-    @Transactional
-    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroTopico dados, UriComponentsBuilder uriBuilder) {
-
-        Topico topicos = repository.save(new Topico(dados));
-        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topicos.getId()).toUri();
-        return ResponseEntity.created(uri).body(new DadosDetalhamentoTopico(topicos));
+    public ResponseEntity<DadosDetalhamentoTopico> cadastrar(@RequestBody @Valid DadosCadastroTopico dados, UriComponentsBuilder uriBuilder) {
+        DadosDetalhamentoTopico dadosDetalhamentoTopico = service.cadastrar(dados);
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(dadosDetalhamentoTopico.id()).toUri();
+        return ResponseEntity.created(uri).body(dadosDetalhamentoTopico);
     }
 
     @GetMapping
-    @Transactional(readOnly = true)
     public ResponseEntity<Page<DadosListagemTopico>> listar(@PageableDefault(sort = {"dataCriacao"}) Pageable pageable) {
-        Page<DadosListagemTopico> page = repository.findAll(pageable).map(DadosListagemTopico::new);
+        Page<DadosListagemTopico> page = service.listar(pageable);
         return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
-    @Transactional(readOnly = true)
-    public ResponseEntity detalhar(@PathVariable Long id) {
-        Topico topico = repository.getReferenceById(id);
-        return ResponseEntity.ok(new DadosDetalhamentoTopico(topico));
+    public ResponseEntity<DadosDetalhamentoTopico> detalhar(@PathVariable Long id) {
+        DadosDetalhamentoTopico dadosDetalhamentoTopico = service.detalhar(id);
+        return ResponseEntity.ok(dadosDetalhamentoTopico);
     }
 }
