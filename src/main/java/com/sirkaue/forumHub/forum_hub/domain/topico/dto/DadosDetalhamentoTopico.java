01fecc1b0306f7cb82a08589b49d6da0715137e4
sirkaue <kauekdslopes@gmail.com>
2024-07-13 11:47:54 -0300

    refatorando dto

--- src/main/java/com/sirkaue/forumHub/forum_hub/domain/topico/dto/DadosDetalhamentoTopico.java
+++ src/main/java/com/sirkaue/forumHub/forum_hub/domain/topico/dto/DadosDetalhamentoTopico.java
@@ -2,18 +2,22 @@ package com.sirkaue.forumHub.forum_hub.domain.topico.dto;
 
 import com.sirkaue.forumHub.forum_hub.domain.topico.Topico;
 
+import java.util.List;
+
 public record DadosDetalhamentoTopico(
         Long id,
         String titulo,
         String curso,
         String mensagem,
-        String autor) {
+        String autor,
+        List<String> respostas) {
 
     public DadosDetalhamentoTopico(Topico topico) {
         this(topico.getId(),
                 topico.getTitulo(),
                 topico.getCurso(),
                 topico.getMensagem(),
-                topico.getAutor());
+                topico.getAutor(),
+                topico.getRespostas());
     }
 }
--- src/main/java/com/sirkaue/forumHub/forum_hub/domain/topico/dto/DadosListagemTopico.java
+++ src/main/java/com/sirkaue/forumHub/forum_hub/domain/topico/dto/DadosListagemTopico.java
@@ -1,18 +1,20 @@
 package com.sirkaue.forumHub.forum_hub.domain.topico.dto;
 
-import com.sirkaue.forumHub.forum_hub.domain.topico.Status;
 import com.sirkaue.forumHub.forum_hub.domain.topico.Topico;
+import com.sirkaue.forumHub.forum_hub.domain.topico.enums.Status;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-public record DadosListagemTopico(Long id,
+public record DadosListagemTopico(
+        Long id,
         String titulo,
         String mensagem,
         LocalDateTime dataCriacao,
         Status status,
         String autor,
-        String curso
-) {
+        String curso) {
+
     public DadosListagemTopico(Topico topico) {
         this(topico.getId(),
                 topico.getTitulo(),
